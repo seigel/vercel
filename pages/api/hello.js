2e55b1d9a64220bcbd7c47dedb18bb3109edad62
James Seigel <github@paddlethis.com>
2020-09-21 13:47:30 -0600

    Some debug logging to see if debug loggin will work

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
+  console.log(req.body);
+  console.log(req.headers);
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
