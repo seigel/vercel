ef16fc5301831ffd299715706732911961cc1e09
James Seigel <github@paddlethis.com>
2020-09-25 10:54:40 -0600

    Trying couch as a store...less technical sprawl

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-const MongoClient = require('mongodb').MongoClient;
-const mongoUri = process.env.MONGO_URI;
+import PouchDB from 'pouchdb';
+const couchURI = process.env.COUCH_URI;
 
 export default async (req, res) => {
     return new Promise(resolve => {
@@ -9,46 +8,21 @@ export default async (req, res) => {
     });
 }
 
-function datadump(req, res, resolve) {
-    const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-    client.connect(err => {
-        const collection = client.db("g_commerce").collection("orders");
-        collection.find({}).toArray().then(data => {
-            console.log(data);
-            console.log(req.body);
-            console.log(req.headers);
-            res.statusCode = 200;
-            res.json({name: data});
-        }).catch(e => {
-            console.log(e);
-            console.log(req.body);
-            console.log(req.headers);
-            res.statusCode = 200;
-            res.json({name: 'not cool'});
-        }).finally(() => {
-            client.close();
-            return resolve();
-        });
-    });
-}
 function saveOrder(req, res, resolve) {
     const {body} = req;
-
-    const client = new MongoClient(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true});
-    client.connect(err => {
-        const collection = client.db("g_commerce").collection("orders");
-        collection.insertOne(body).then( value =>  {
-            res.statusCode = 200;
-            res.json({success: true, value: value});
-        }).catch(e => {
-            console.log(e);
-            console.log(req.body);
-            console.log(req.headers);
-            res.statusCode = 200;
-            res.json({success: false});
-        }).finally(() => {
-            client.close();
-            return resolve();
-        });
+    const client = PouchDB(couchURI);
+    body['_id'] = `test:${body.orderId}`;
+    client.put(body).then((value) => {
+        res.statusCode = 200;
+        res.json({success: true});
+    }).catch(e => {
+        console.log(e);
+        console.log(body);
+        console.log(req.headers);
+        res.statusCode = 200;
+        res.json({success: false});
+    }).finally(() => {
+        client.close();
+        return resolve();
     });
 }
